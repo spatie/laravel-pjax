c30634de9eb2169db0dabdebff56d5d7532d52ca
CupOfTea696 <cupoftea696@gmail.com>
2016-01-11 17:45:01 +0000

    Set Pjax Version header if available

--- src/Middleware/FilterIfPjax.php
+++ src/Middleware/FilterIfPjax.php
@@ -26,7 +26,8 @@ class FilterIfPjax
         }
 
         $this->filterResponse($response, $request->header('X-PJAX-CONTAINER'))
-            ->setUriHeader($response, $request);
+            ->setUriHeader($response, $request)
+            ->setVersionHeader($response, $request);
 
         return $response;
     }
@@ -39,7 +40,7 @@ class FilterIfPjax
      */
     protected function filterResponse(Response $response, $container)
     {
-        $crawler = new Crawler($response->getContent());
+        $crawler = $this->getCrawler($response);
 
         $response->setContent(
             $this->makeTitle($crawler).
@@ -85,5 +86,38 @@ class FilterIfPjax
     protected function setUriHeader(Response $response, Request $request)
     {
         $response->header('X-PJAX-URL', $request->getRequestUri());
+
+        return $this;
+    }
+
+    /**
+     * @param \Illuminate\Http\Response $response
+     * @param \Illuminate\Http\Request  $request
+     */
+    protected function setVersionHeader(Response $response, Request $request)
+    {
+        $crawler = $this->getCrawler($response);
+        $node = $crawler->filter('head > meta[http-equiv]');
+
+        if ($node->count()) {
+            $response->header('X-PJAX-VERSION', $node->attr('content'));
+        }
+
+        return $this;
+    }
+
+    /**
+     * Get the DomCrawler instance.
+     *
+     * @param \Illuminate\Http\Response $response
+     * @return \Symfony\Component\DomCrawler\Crawler
+     */
+    protected function getCrawler(Response $response)
+    {
+        if ($this->crawler) {
+            return $this->crawler;
+        }
+
+        return new Crawler($response->getContent());
     }
 }
